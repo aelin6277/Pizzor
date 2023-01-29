42fcd3563f2d7b61d603d3c6b6331419f01b35f6
elina <aelin6277@gmail.com>
2023-01-29 23:43:14 +0100

    Initial Commit

---
+++ Pizzor/PizzorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzorApp.swift
+//  Pizzor
+//
+//  Created by Elin A on 2023-01-29.
+//
+
+import SwiftUI
+
+@main
+struct PizzorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
